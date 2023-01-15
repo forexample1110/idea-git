57d58b3895c9afb4f788edf9ed1f530a2a339b44
ljx <ly188270@163.com>
2023-01-15 23:38:17 +0800

    second commit

--- com/git/gittest.java
+++ com/git/gittest.java
@@ -4,5 +4,6 @@ package com.git;
 public class gittest {
     public static void main(String[] args) {
         System.out.println("1111111111111");
+        System.out.println("2222222222222");
     }
 }
