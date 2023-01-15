ec9c61ad338cc53de7dfd7129177f42af573c1fe
ljx <ly188270@163.com>
2023-01-15 23:45:36 +0800

    master test commit

--- com/git/gittest.java
+++ com/git/gittest.java
@@ -7,5 +7,6 @@ public class gittest {
         System.out.println("2222222222222");
         System.out.println("3333333333333");
         System.out.println("hot-fix no conflict merge");
+        System.out.println("master test");
     }
 }
