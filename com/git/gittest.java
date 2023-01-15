e5250cdf0585f3d5160738439c571c3966c8437f
ljx <ly188270@163.com>
2023-01-15 23:46:57 +0800

    hot-fix test commit

--- com/git/gittest.java
+++ com/git/gittest.java
@@ -4,8 +4,10 @@ package com.git;
 public class gittest {
     public static void main(String[] args) {
         System.out.println("1111111111111");
+        System.out.println("=====================");
         System.out.println("2222222222222");
         System.out.println("3333333333333");
         System.out.println("hot-fix no conflict merge");
+        System.out.println("hot-fix test");
     }
 }
