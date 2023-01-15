13d699ecb12dffa440669f227bc018d8468b165c
ljx <ly188270@163.com>
2023-01-15 23:40:52 +0800

    hot-fix first commit

--- com/git/gittest.java
+++ com/git/gittest.java
@@ -6,5 +6,6 @@ public class gittest {
         System.out.println("1111111111111");
         System.out.println("2222222222222");
         System.out.println("3333333333333");
+        System.out.println("hot-fix no conflict merge");
     }
 }
